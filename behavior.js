6e15fc66b3cd8290a2f86c5c1e822d93681c0c66
Alejandro <chapale2000@gmail.com>
2024-11-01 20:02:51 -0300

    final version 1.0

--- behavior.js
+++ behavior.js
@@ -1,3 +1,96 @@
+const buttons = document.querySelectorAll("button");
+const result = document.querySelector(".results");
+let firstNumber = undefined;
+let operator = undefined;
+let secondNumber = undefined;
+let banderita = true; // bandera que permite ingresar nuevo numero en la calculadora o no
+
+buttons.forEach((buttons) => {
+    buttons.addEventListener("click", () => {
+        let value;
+        switch (buttons.className) {
+            case "zero" : value = "0";
+            break;
+            case "one": value = "1"; 
+            break;
+            case "two": value = "2";
+            break;
+            case "three": value = "3";
+            break;
+            case "four": value = "4";
+            break;
+            case "five": value = "5";
+            break;
+            case "six": value = "6"; 
+            break;
+            case "seven": value = "7"; 
+            break;
+            case "eight": value = "8"; 
+            break;
+            case "nine": value = "9"; 
+            break;
+            case "dot": value = ".";
+            break;
+            case "backspace": value = "b";
+            break;
+            case "clear": 
+                result.textContent = "0";
+                firstNumber = undefined; 
+                secondNumber = undefined; 
+                operator = undefined; 
+                banderita = true;
+            break;
+            case "equals":
+                if (firstNumber != undefined) {
+                    secondNumber = parseFloat(result.textContent);
+                    result.textContent = (operate(firstNumber, operator, secondNumber)).toString();
+                }
+                firstNumber = undefined;
+                secondNumber = undefined;
+                operator = undefined;
+                banderita = true;
+            break;
+            default:
+                if (firstNumber == undefined) {
+                    firstNumber = parseFloat(result.textContent);
+                    operator = buttons.className;
+                }
+                else {
+                    secondNumber = parseFloat(result.textContent); 
+                    firstNumber = operate(firstNumber, operator, secondNumber);
+                    result.textContent = (firstNumber).toString();
+                    secondNumber = undefined;
+                    operator = buttons.className;
+                }
+                banderita = true;
+            break;
+        }
+
+        
+        if (!isNaN(parseFloat(value))) {
+            
+            if (banderita || result.textContent == "0") {
+                result.textContent = value; 
+                banderita = false;
+            }  
+            else result.textContent += value;  
+            
+        } 
+        
+        if (value == "." && !(result.textContent.includes("."))) {
+            result.textContent += value;
+        }
+        
+        if (value == "b" && result.textContent != "0") {
+            let arr = result.textContent.split("");
+            arr.pop();
+            result.textContent = arr.join("");
+            if (arr.length < 1) result.textContent = "0";
+        } 
+        
+    });
+});
+
 function add(a, b) {
     return a+b;
 }
@@ -14,13 +107,12 @@ function divide(a, b){
     return a/b;
 }
 
-let firstNumber, operator, secondNumber;
-
 function operate(first, operator, second) {
     switch(operator){
-        case "+": return add(first, second);
-        case "-": return substract(first, second);
-        case "*": return multiply(first, second);
-        case "/": return divide(first, second);
+        case "add": return add(first, second);
+        case "substract": return substract(first, second);
+        case "multiply": return multiply(first, second);
+        case "divide": return  divide(first, second);
     }
 }
+
